d3d3ee8b2bd62a62491c70e8b5c970a4403ea3d5
BobBaguetti <extrudevertices@gmail.com>
2025-04-15 14:12:59 +0300

    fire

--- scripts/script.js
+++ scripts/script.js
@@ -5,12 +5,13 @@ document.addEventListener("DOMContentLoaded", () => {
   // Firebase Setup (Replace placeholders with your config)
   // ===========================
   const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "yourproject.firebaseapp.com",
-    projectId: "yourproject",
-    storageBucket: "yourproject.appspot.com",
-    messagingSenderId: "YOUR_SENDER_ID",
-    appId: "YOUR_APP_ID"
+    apiKey: "AIzaSyDwEdPK3UdPN5MB8YAuM_jb0K1iXfQ-tGQ",
+    authDomain: "vbmap-cc834.firebaseapp.com",
+    projectId: "vbmap-cc834",
+    storageBucket: "vbmap-cc834.firebasestorage.app",
+    messagingSenderId: "244112699360",
+    appId: "1:244112699360:web:95f50adb6e10b438238585",
+    measurementId: "G-7FDNWLRM95"
   };
   firebase.initializeApp(firebaseConfig);
   const db = firebase.firestore();
