df75ab96b70d67d23ddcbddd5e11824caa19e446
BobBaguetti <extrudevertices@gmail.com>
2025-04-18 12:26:04 +0300

    import fixes for leaflet

--- scripts/modules/map/markerManager.js
+++ scripts/modules/map/markerManager.js
@@ -3,7 +3,7 @@
 // @version: 2
 // @file:    /scripts/modules/map/markerManager.js
 
-import L from 'leaflet';
+import * from 'leaflet';
 import { openEmptyMarkerForm, openMarkerFormWithData } from '../ui/markerForm.js';
 import { formatRarity } from '../utils/utils.js';
 
