424d016f122576ef5f6d3b4d12229e6e39fb9228
BobBaguetti <extrudevertices@gmail.com>
2025-04-23 20:08:37 +0300

    readjusting rows

--- scripts/modules/ui/forms/builders/itemFormBuilder.js
+++ scripts/modules/ui/forms/builders/itemFormBuilder.js
@@ -1,5 +1,5 @@
-// @version: 1
-// @file: /scripts/modules/forms/itemFormBuilder.js
+// @version: 2
+// @file: /scripts/modules/forms/builders/itemFormBuilder.js
 
 import {
   createNameField,
@@ -10,51 +10,53 @@ import {
   createQuantityField,
   createImageFieldSet,
   createExtraInfoField
-} from "../universalForm.js";
-  
-  export function createItemForm() {
-    const form = document.createElement("form");
-    form.id = "item-form";
-  
-    const { row: rowName, input: fldName, colorBtn: colorName } = createNameField();
-    const { row: rowType, select: fldType, colorBtn: colorType } = createItemTypeField();
-    const { row: rowRarity, select: fldRarity, colorBtn: colorRarity } = createRarityField();
-    const { row: rowDesc, textarea: fldDesc, colorBtn: colorDesc } = createDescriptionField();
-    const { row: rowValue, input: fldValue, colorBtn: colorValue } = createValueField();
-    const { row: rowQty, input: fldQty, colorBtn: colorQty } = createQuantityField();
-    const { row: rowExtras, extraInfo } = createExtraInfoField();
-    const {
-      rowImgS, fldImgS,
-      rowImgL, fldImgL,
-      rowVid, fldVid
-    } = createImageFieldSet();
-  
-    form.append(
-      rowName,
-      rowType,
-      rowRarity,
-      rowDesc,
-      rowValue,
-      rowQty,
-      rowExtras,
-      rowImgS,
-      rowImgL,
-      rowVid
-    );
-  
-    return {
-      form,
-      fields: {
-        fldName, colorName,
-        fldType, colorType,
-        fldRarity, colorRarity,
-        fldDesc, colorDesc,
-        fldValue, colorValue,
-        fldQty, colorQty,
-        fldImgS, fldImgL, fldVid,
-        extraInfo,
-        rowExtras 
-      }
-    };
-  }
-  
+} from "./universalForm.js";
+
+export function createItemForm() {
+  const form = document.createElement("form");
+  form.id = "item-form";
+
+  const { row: rowName, input: fldName, colorBtn: colorName } = createNameField();
+  const { row: rowType, select: fldType, colorBtn: colorType } = createItemTypeField();
+  const { row: rowRarity, select: fldRarity, colorBtn: colorRarity } = createRarityField();
+  const { row: rowDesc, textarea: fldDesc, colorBtn: colorDesc } = createDescriptionField();
+  const { row: rowExtras, extraInfo } = createExtraInfoField();
+  const { row: rowValue, input: fldValue, colorBtn: colorValue } = createValueField();
+  const { row: rowQty, input: fldQty, colorBtn: colorQty } = createQuantityField();
+  const {
+    rowImgS, fldImgS,
+    rowImgL, fldImgL
+  } = createImageFieldSet();
+
+  // Insert <hr> elements
+  const hrAboveExtras = document.createElement("hr");
+  const hrBelowExtras = document.createElement("hr");
+
+  form.append(
+    rowName,
+    rowType,
+    rowRarity,
+    rowDesc,
+    hrAboveExtras,
+    rowExtras,
+    hrBelowExtras,
+    rowValue,
+    rowQty,
+    rowImgS,
+    rowImgL
+  );
+
+  return {
+    form,
+    fields: {
+      fldName, colorName,
+      fldType, colorType,
+      fldRarity, colorRarity,
+      fldDesc, colorDesc,
+      fldValue, colorValue,
+      fldQty, colorQty,
+      fldImgS, fldImgL,
+      extraInfo
+    }
+  };
+}
