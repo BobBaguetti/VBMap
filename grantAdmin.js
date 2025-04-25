2a0ff50d940da2bf4db025ff70339ed259506a05
BobBaguetti <extrudevertices@gmail.com>
2025-04-25 18:16:12 +0000

    ignore

--- grantAdmin.js
+++ grantAdmin.js
@@ -1,16 +1,22 @@
 // grantAdmin.js
+// @version: 2
 
 const admin = require("firebase-admin");
+const path  = require("path");
 
-// Use your own service account key or Application Default Credentials
+// Load your downloaded key (which is git-ignored)
+const serviceAccount = require(path.join(__dirname, "serviceAccountKey.json"));
+
 admin.initializeApp({
-  credential: admin.credential.applicationDefault()
+  credential: admin.credential.cert(serviceAccount),
+  projectId:  "vbmap-cc834"    // ← your Firebase project ID
 });
 
-// Replace with the UID of the user you just created
+// Replace with the UID of the user you created in Authentication
 const uid = "VLWG0Z1amSWZK5jlrFhy1q5mt1N2";
 
-admin.auth().setCustomUserClaims(uid, { admin: true })
+admin.auth()
+  .setCustomUserClaims(uid, { admin: true })
   .then(() => {
     console.log(`✅ Granted admin to ${uid}`);
     process.exit(0);
