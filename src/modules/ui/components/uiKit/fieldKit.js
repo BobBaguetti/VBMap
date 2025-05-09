e0b7ef17fc1d1de0cc45a58d57b6ecd298f2e8a9
BobBaguetti <extrudevertices@gmail.com>
2025-05-09 08:36:35 +0300

    s

--- src/modules/ui/components/uiKit/fieldKit.js
+++ src/modules/ui/components/uiKit/fieldKit.js
@@ -100,5 +100,4 @@ export function createVideoField(labelText, id) {
 }
 
 // Re-export the extra-info block factory
-export { createExtraInfoBlock } from "./extraInfoBlock.js";
 export { createExtraInfoRow } from "./extraInfoBlock.js";
