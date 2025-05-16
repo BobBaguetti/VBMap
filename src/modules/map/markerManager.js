bd69048a940f1ff834f7c6ce53f24689e76f88a9
BobBaguetti <extrudevertices@gmail.com>
2025-05-16 07:18:40 +0300

    s

--- src/modules/map/markerManager.js
+++ src/modules/map/markerManager.js
@@ -1,20 +1,20 @@
 /* @file: src/modules/map/markerManager.js */
-/* @version: 13.1 — re-export color presets for form controllers */
+/* @version: 13.2 — fixed default vs. named exports for marker modules */
 
-import { createMarker }       from "./markers/common/createMarker.js";
-import { createCustomIcon }   from "./markers/common/createCustomIcon.js";
-import { wrapPopup }          from "./markers/common/popupBase.js";
+import createMarker         from "./markers/common/createMarker.js";
+import createCustomIcon     from "./markers/common/createCustomIcon.js";
+import { wrapPopup }        from "./markers/common/popupBase.js";
 
-import createChestMarker      from "./markers/chest/factory.js";
-import renderChestPopup       from "./markers/chest/popup.js";
+import createChestMarker    from "./markers/chest/factory.js";
+import renderChestPopup     from "./markers/chest/popup.js";
 
-import createItemMarker       from "./markers/item/factory.js";
-import renderItemPopup        from "./markers/item/popup.js";
+import createItemMarker     from "./markers/item/factory.js";
+import renderItemPopup      from "./markers/item/popup.js";
 
-import createNPCMarker        from "./markers/npc/factory.js";
-import renderNPCPopup         from "./markers/npc/popup.js";
+import createNPCMarker      from "./markers/npc/factory.js";
+import renderNPCPopup       from "./markers/npc/popup.js";
 
-import { CHEST_RARITY }       from "./markers/utils.js";
+import { CHEST_RARITY }     from "./markers/utils.js";
 import { rarityColors, defaultNameColor } from "../utils/colorPresets.js";
 
 // Re-exports
