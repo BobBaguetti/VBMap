0974f47783c8432798ea923615554e6c6dd89f0f
BobBaguetti <extrudevertices@gmail.com>
2025-05-16 08:51:22 +0300

    s

--- src/modules/marker/types.js
+++ src/modules/marker/types.js
@@ -1,5 +1,5 @@
 // @file: src/modules/marker/types.js
-// @version: 1.0 — registry of marker-type configurations for services, forms, modals, popups, icons, and filters
+// @version: 1.1 — corrected formBuilder imports to match actual exports
 
 import {
   loadItemDefinitions,
@@ -19,8 +19,9 @@ import {
 import { initItemDefinitionsModal } from "../ui/modals/itemDefinitionsModal.js";
 import { initChestDefinitionsModal } from "../ui/modals/chestDefinitionsModal.js";
 
-import { itemFormBuilder } from "../ui/forms/builders/itemFormBuilder.js";
-import { chestFormBuilder } from "../ui/forms/builders/chestFormBuilder.js";
+// Import builder functions by their actual exported names
+import { createItemForm } from "../ui/forms/builders/itemFormBuilder.js";
+import { createChestForm } from "../ui/forms/builders/chestFormBuilder.js";
 
 import { itemFormController } from "../ui/forms/controllers/itemFormController.js";
 import { chestFormController } from "../ui/forms/controllers/chestFormController.js";
@@ -30,26 +31,26 @@ import { setupChestFilters } from "../sidebar/filters/chestFilters.js";
 
 export const markerTypes = {
   Item: {
-    loadDefinitions: loadItemDefinitions,
-    subscribeDefinitions: subscribeItemDefinitions,
-    formBuilder: itemFormBuilder,
-    formController: itemFormController,
-    modalInit: initItemDefinitionsModal,
-    popupRenderer: renderItemPopup,
-    iconFactory: createCustomIcon,
-    filterSetup: setupItemFilters,
-    showInSidebar: def => def.showInFilters,
+    loadDefinitions:        loadItemDefinitions,
+    subscribeDefinitions:   subscribeItemDefinitions,
+    formBuilder:            createItemForm,      // was itemFormBuilder
+    formController:         itemFormController,
+    modalInit:              initItemDefinitionsModal,
+    popupRenderer:          renderItemPopup,
+    iconFactory:            createCustomIcon,
+    filterSetup:            setupItemFilters,
+    showInSidebar:          def => def.showInFilters,
   },
   Chest: {
-    loadDefinitions: loadChestDefinitions,
-    subscribeDefinitions: subscribeChestDefinitions,
-    formBuilder: chestFormBuilder,
-    formController: chestFormController,
-    modalInit: initChestDefinitionsModal,
-    popupRenderer: renderChestPopup,
-    iconFactory: createCustomIcon,
-    filterSetup: setupChestFilters,
-    showInSidebar: () => true,
+    loadDefinitions:        loadChestDefinitions,
+    subscribeDefinitions:   subscribeChestDefinitions,
+    formBuilder:            createChestForm,     // was chestFormBuilder
+    formController:         chestFormController,
+    modalInit:              initChestDefinitionsModal,
+    popupRenderer:          renderChestPopup,
+    iconFactory:            createCustomIcon,
+    filterSetup:            setupChestFilters,
+    showInSidebar:          () => true,
   },
   // Future types (NPC, Quest, Secret, Misc) can be added here
 };
