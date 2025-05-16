7e1970ca3f396b6ee0b32121c403d667060890f8
BobBaguetti <extrudevertices@gmail.com>
2025-05-16 16:42:26 +0300

    s

--- src/modules/ui/components/definitionListManager.js
+++ src/modules/ui/components/definitionListManager.js
@@ -90,6 +90,7 @@ export function createDefinitionListManager({
       fontSize:       "0.9em",
       color:          "var(--text-secondary)",
       flex:           "1 1 auto",
+      maxWidth:       "60%",
       minWidth:       "0",
       whiteSpace:     "nowrap",
       overflow:       "hidden",
