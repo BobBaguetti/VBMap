16aa108722badec123fbb32effcb71dd82e1269a
BobBaguetti <extrudevertices@gmail.com>
2025-05-17 11:18:42 +0300

    s

--- src/shared/ui/index.js
+++
@@ -1,8 +0,0 @@
-// @file:    src/shared/ui/index.js
-// @version: 1
-
-export * from "./components";
-export * from "./forms";
-export * from "./managers";
-export * from "./core";
-export * from "./context-menu";
