48aca3cc8848674366f897960f89e95e44a903ed
BobBaguetti <extrudevertices@gmail.com>
2025-05-17 10:02:31 +0300

    s

--- src/shared/ui/index.js
+++ src/shared/ui/index.js
@@ -1,8 +1,8 @@
-// @file:    src/shared/ui/index.js
-// @version: 1
+// @file: src/shared/ui/index.js
+// @version: 1.1 — use explicit index.js filenames
 
-export * from "./components";
-export * from "./forms";
-export * from "./managers";
-export * from "./core";
-export * from "./context-menu";
+export * from "./components/index.js";
+export * from "./forms/index.js";
+export * from "./managers/index.js";
+export * from "./core/index.js";
+export * from "./context-menu/index.js";
