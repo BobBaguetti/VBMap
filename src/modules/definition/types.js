92b150be05da51c8549a2d69fc076441842602f2
BobBaguetti <extrudevertices@gmail.com>
2025-05-19 08:47:37 +0300

    s

--- src/modules/definition/types.js
+++ src/modules/definition/types.js
@@ -20,14 +20,13 @@ import {
 import { itemSchema }  from "./schemas/itemSchema.js";
 import { chestSchema } from "./schemas/chestSchema.js";
 
-import { buildForm }            from "./forms/definitionFormBuilder.js";
 import { createFormController } from "./forms/definitionFormController.js";
+import { buildForm }            from "./forms/definitionFormBuilder.js";
 import { createPreviewController } from "./preview/previewController.js";
 
 export const definitionTypes = {
   Item: {
     schema:    itemSchema,
-    // Data methods
     loadDefs:  loadItemDefinitions,
     subscribe: subscribeItemDefinitions,
     save:      (db, id, data) =>
@@ -35,7 +34,6 @@ export const definitionTypes = {
                    ? updateItemDefinition(db, id, data)
                    : createItemDefinition(db, data),
     del:       deleteItemDefinition,
-    // UI hooks
     controller: (handlers, db) =>
                   createFormController(buildForm(itemSchema), itemSchema, handlers),
     previewBuilder: host =>
