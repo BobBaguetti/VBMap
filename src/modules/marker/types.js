18b277393542e5eda06192205d6e6c6529c50537
BobBaguetti <extrudevertices@gmail.com>
2025-05-22 13:53:25 +0300

    s

--- src/modules/marker/types.js
+++ src/modules/marker/types.js
@@ -1,45 +1,43 @@
 // @file: src/modules/marker/types.js
-// @version: 1.5 — switch to unified definition schema & modal
+// @version: 1.6 — use service loaders directly to avoid circular import
 
 import {
   loadItemDefinitions,
-  subscribeItemDefinitions,
+  subscribeItemDefinitions
 } from "../services/itemDefinitionsService.js";
 import {
   loadChestDefinitions,
-  subscribeChestDefinitions,
+  subscribeChestDefinitions
 } from "../services/chestDefinitionsService.js";
 
 import {
   renderItemPopup,
   renderChestPopup,
-  createCustomIcon,
+  createCustomIcon
 } from "../map/markerManager.js";
-
-import { definitionTypes } from "../definition/types.js";
 import { setupItemFilters }  from "../sidebar/filters/itemFilters.js";
 import { setupChestFilters } from "../sidebar/filters/chestFilters.js";
 
 export const markerTypes = {
   Item: {
     defIdKey:             "predefinedItemId",
-    loadDefinitions:      definitionTypes.Item.loadDefs,
-    subscribeDefinitions: definitionTypes.Item.subscribe,
-    // we no longer have per-type modals or form builders/controllers here
+    loadDefinitions:      loadItemDefinitions,
+    subscribeDefinitions: subscribeItemDefinitions,
     popupRenderer:        renderItemPopup,
     iconFactory:          createCustomIcon,
     filterSetup:          setupItemFilters,
-    showInSidebar:        def => def.showInFilters,
+    showInSidebar:        def => def.showInFilters
   },
 
   Chest: {
     defIdKey:             "chestTypeId",
-    loadDefinitions:      definitionTypes.Chest.loadDefs,
-    subscribeDefinitions: definitionTypes.Chest.subscribe,
+    loadDefinitions:      loadChestDefinitions,
+    subscribeDefinitions: subscribeChestDefinitions,
     popupRenderer:        renderChestPopup,
     iconFactory:          createCustomIcon,
     filterSetup:          setupChestFilters,
-    showInSidebar:        () => true,
+    showInSidebar:        () => true
   }
-  // … future types can be added here …
+
+  // …add future types here…
 };
