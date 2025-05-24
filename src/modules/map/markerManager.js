90268056b113996c666cbc2c54be93462acbd493
BobBaguetti <extrudevertices@gmail.com>
2025-05-24 13:13:15 +0300

    s

--- src/modules/map/markerManager.js
+++ src/modules/map/markerManager.js
@@ -1,9 +1,10 @@
 // @file: src/modules/map/markerManager.js
-// @version: 11.0 — split out popups, icons, utils into dedicated modules
+// @version: 11.1 — added NPC popup export
 
 import { createMarker }      from "./marker/icons/createMarker.js";
 import { renderItemPopup }   from "./marker/popups/itemPopup.js";
 import { renderChestPopup }  from "./marker/popups/chestPopup.js";
+import { renderNpcPopup }    from "./marker/popups/npcPopup.js";
 import { createCustomIcon }  from "./marker/icons/createCustomIcon.js";
 import { CHEST_RARITY }      from "./marker/utils.js";
 import { rarityColors, defaultNameColor } from "../../shared/utils/color/colorPresets.js";
@@ -12,6 +13,7 @@ export {
   createMarker,
   renderItemPopup,
   renderChestPopup,
+  renderNpcPopup,        
   createCustomIcon,
   CHEST_RARITY,
   rarityColors,
